6605614794625846467a633296ecaba35a32e503
Louis Low <iamlouislow@gmail.com>
2022-07-28 22:38:43 +0800

    clean up

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import 'weavv-css/dist/weavv-1.2.0.min.css';
+import 'weavv-css/dist/weavv-1.2.1.min.css';
 
 Vue.config.productionTip = false;
 
